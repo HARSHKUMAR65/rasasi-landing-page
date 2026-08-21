cd6e137d2eb4c430d2f3da323f2ae1a7b5628f78
HARSHKUMAR65 <kabiryadav55555@gmail.com>
2026-08-21 19:25:52 +0530

    Add Google Tag Manager

--- index.php
+++ index.php
@@ -2,6 +2,13 @@
 <html lang="en">
 
 <head>
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-PJ4ZBT6S');</script>
+  <!-- End Google Tag Manager -->
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="Explore the World of Hawas by Rasasi at ASD Market Week. Discover the iconic Hawas fragrance collection and experience Rasasi at this premier retail event.">
@@ -25,6 +32,10 @@
 </head>
 
 <body>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PJ4ZBT6S"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
   <svg class="svg-sprite" aria-hidden="true">
     <defs>
       <linearGradient id="gold-gradient" x1="0%" y1="0%" x2="100%" y2="0%">
